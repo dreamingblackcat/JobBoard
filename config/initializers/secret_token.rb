44769f597735686ea9c4c0b9b9e657eedd913b86
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2013-09-21 10:19:38 +0630

    initial commit with a barebone rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobMatchBasic::Application.config.secret_token = 'dad1bea8adf4cfc9904d64415ef710144ebce0da067487db907158152837daec9a096a8d63539dfcd2d5f5592f7517b2d2992a52ddc668c50e39db1403414eab'
