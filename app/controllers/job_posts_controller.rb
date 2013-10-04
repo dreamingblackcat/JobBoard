59bfb5b16972f80ac46acf1f0b7dbad386232b97
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2013-10-04 16:56:57 +0630

    company and job post paths fixed

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -34,4 +34,32 @@ class JobPostsController < ApplicationController
     @job_post = @company.job_posts.create(params[:job_post])
     redirect_to [@company,@job_post]
   end
+  def edit
+    @company = Company.find(params[:company_id])
+    @job_post=@company.job_posts.find(params[:id])
+    
+  end
+  def update
+    @company = Company.find(params[:company_id])
+    @job_post=@company.job_posts.find(params[:id])
+    respond_to do |format|
+      if @job_post.update_attributes(params[:job_post])
+        format.html { redirect_to [@job_post.company,@job_post], notice: 'Company was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @job_post=JobPost.find(params[:id])
+    @job_post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to job_posts_url }
+      format.json { head :no_content }
+    end
+  end
+    
 end
